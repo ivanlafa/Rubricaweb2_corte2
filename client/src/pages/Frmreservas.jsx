7c53e84170e56cada17f84fa147c8fbcbff1ef4b
ivanlafa <ivanlafa98@gmail.com>
2023-05-25 17:34:50 -0500

    Cambios -> reservas  consola

--- client/src/pages/Frmreservas.jsx
+++ client/src/pages/Frmreservas.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import "../styless/styless.css";
-import { Formik, Form } from "formik";
+import { Formik, Form, Field } from "formik";
 
 const FrmReservas = () => {
   const [codigo, setCodigo] = useState("");
@@ -12,102 +12,60 @@ const FrmReservas = () => {
   const [fechaEntrada, setFechaEntrada] = useState("");
   const [fechaSalida, setFechaSalida] = useState("");
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // Lógica de envío del formulario o llamada a una función de manejo de datos
+  const handleSubmit = (values) => {
+    setCodigo(values.codigo);
+    setCodigoHabitacion(values.codigoHabitacion);
+    setNombreCliente(values.nombreCliente);
+    setTelefonoCliente(values.telefonoCliente);
+    setFechaReservacion(values.fechaReservacion);
+    setFechaEntrada(values.fechaEntrada);
+    setFechaSalida(values.fechaSalida);
+
+    console.log('datos del formulario ', values)
   };
 
   return (
     <div className="formulario-container">
-      <Formik
-        initialValues={{
-          codigo: "",
-          codigo_habitacion: "",
-          nombre_cliente: "",
-          telefono_cliente: "",
-          fecha_reservacion: "",
-          fecha_entrada: "",
-          fecha_salida: ""
-        }}
-        onSubmit={handleSubmit}
-      >
-        <Form>
-          <h2>Crear Reservas</h2>
+    <Formik
+      initialValues={{
+        codigo: codigo,
+        codigoHabitacion: codigoHabitacion,
+        nombreCliente: nombreCliente,
+        telefonoCliente: telefonoCliente,
+        fechaReservacion: fechaReservacion,
+        fechaEntrada:fechaEntrada,
+        fechaSalida:fechaSalida,
+      }}
+      onSubmit={handleSubmit}
+    >
+      <Form>
+        <h2>Crear Reservas</h2>
 
-          <label htmlFor="codigo">Código:</label>
-          <input
-            type="text"
-            id="codigo"
-            name="codigo"
-            value={codigo}
-            onChange={(e) => setCodigo(e.target.value)}
-            required
-          />
+        <label htmlFor="codigo">Código:</label>
+        <Field type="text" id="codigo" name="codigo" />
 
-          <label htmlFor="codigo_habitacion">Código de Habitación:</label>
-          <input
-            type="text"
-            id="codigo_habitacion"
-            name="codigo_habitacion"
-            value={codigoHabitacion}
-            onChange={(e) => setCodigoHabitacion(e.target.value)}
-            required
-          />
+        <label htmlFor="codigoHabitacion">Codigo Habitacion:</label>
+        <Field type="text" id="codigoHabitacion" name="codigoHabitacion" />
 
-          <label htmlFor="nombre_cliente">Nombre del Cliente:</label>
-          <input
-            type="text"
-            id="nombre_cliente"
-            name="nombre_cliente"
-            value={nombreCliente}
-            onChange={(e) => setNombreCliente(e.target.value)}
-            required
-          />
+        <label htmlFor="nombreCliente">Nombre Cliente:</label>
+        <Field type="text" id="nombreCliente" name="nombreCliente" />
 
-          <label htmlFor="telefono_cliente">Teléfono del Cliente:</label>
-          <input
-            type="tel"
-            id="telefono_cliente"
-            name="telefono_cliente"
-            value={telefonoCliente}
-            onChange={(e) => setTelefonoCliente(e.target.value)}
-            required
-          />
+        <label htmlFor="telefonoCliente">Telefono Cliente:</label>
+        <Field type="text" id="telefonoCliente" name="telefonoCliente" />
 
-          <label htmlFor="fecha_reservacion">Fecha de Reservación:</label>
-          <input
-            type="date"
-            id="fecha_reservacion"
-            name="fecha_reservacion"
-            value={fechaReservacion}
-            onChange={(e) => setFechaReservacion(e.target.value)}
-            required
-          />
+        <label htmlFor="fechaReservacion">Fecha Reservacion:</label>
+        <Field type="date" id="fechaReservacion" name="fechaReservacion" />
 
-          <label htmlFor="fecha_entrada">Fecha de Entrada:</label>
-          <input
-            type="date"
-            id="fecha_entrada"
-            name="fecha_entrada"
-            value={fechaEntrada}
-            onChange={(e) => setFechaEntrada(e.target.value)}
-            required
-          />
+        <label htmlFor="fechaEntrada">Fecha Entrada:</label>
+        <Field type="date" id="fechaEntrada" name="fechaEntrada" />
 
-          <label htmlFor="fecha_salida">Fecha de Salida:</label>
-          <input
-            type="date"
-            id="fecha_salida"
-            name="fecha_salida"
-            value={fechaSalida}
-            onChange={(e) => setFechaSalida(e.target.value)}
-            required
-          />
+        <label htmlFor="fechaSalida">Fecha Salida:</label>
+        <Field type="date" id="fechaSalida" name="fechaSalida" />
 
-          <input type="submit" value="Enviar" />
-        </Form>
-      </Formik>
-    </div>
+        <input type="submit" value="Enviar" />
+      </Form>
+    </Formik>
+  </div>
   );
 };
 
